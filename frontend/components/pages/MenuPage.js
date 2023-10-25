546eaa6bbbed3a7aec24122c5dbbc639a322bfe6
guilhermemestre-devcit <guilherme.mestre@citechnology.com.br>
2023-10-24 21:51:41 -0300

    a

--- frontend/components/pages/MenuPage.js
+++ frontend/components/pages/MenuPage.js
@@ -1,8 +1,32 @@
-import { Text } from "react-native";
+import React from 'react';
+import { View, TextInput, StyleSheet } from 'react-native';
 
-
-export default function MenuPage() {
-    return (
-        <Text>MenuPage</Text>
-    );
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.searchBar}>
+        <TextInput
+          style={styles.input}
+          placeholder="Pesquisar..."
+        />
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 50,
+    paddingHorizontal: 16,
+  },
+  searchBar: {
+    marginBottom: 16,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    paddingLeft: 8,
+  },
+});
