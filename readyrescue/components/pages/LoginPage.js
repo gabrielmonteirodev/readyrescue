61b8d8e3edd9b94c37de5b50cbb7b3ab5a8c6fce
Gabriel Monteiro <matos6542@gmail.com>
2023-12-02 00:43:25 -0300

    Botao de login com o google funcionando

--- readyrescue/components/pages/LoginPage.js
+++ readyrescue/components/pages/LoginPage.js
@@ -1,22 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, Image, StyleSheet, Button } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import logo from '../../assets/logo_ready_rescue.png';
-import { LinearGradient } from 'expo-linear-gradient';
-import { GoogleSignIn, GoogleSigninButton } from '@react-native-google-signin/google-signin';
-import auth from '@react-native-firebase/auth';
-
+import React, { useState, useEffect } from "react";
+import { View, Text, Image, StyleSheet, Button } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import logo from "../../assets/logo_ready_rescue.png";
+import { LinearGradient } from "expo-linear-gradient";
+import { GoogleSigninButton, GoogleSignin } from "@react-native-google-signin/google-signin";
+import auth from "@react-native-firebase/auth";
 
 const LoginPage = () => {
   const [initializing, setInitializing] = useState(true);
   const [user, setUser] = useState(null);
   const navigation = useNavigation();
-  
-  useEffect(()=>{
-    GoogleSignIn.configure({
-      webClientId: '224957591212-6i9213qi53se11kvf7sld1lut0pvah2u.apps.googleusercontent.com'
-    })
-  },[]) 
+
+  GoogleSignin.configure({
+    webClientId:
+      "224957591212-6i9213qi53se11kvf7sld1lut0pvah2u.apps.googleusercontent.com",
+    offlineAccess: true})
 
   function onAuthStateChanged(user) {
     setUser(user);
@@ -32,18 +30,30 @@ const LoginPage = () => {
 
   async function onGoogleButtonPress() {
     try {
-      const { idToken } = await GoogleSignIn.signIn();
+      const { idToken } = await GoogleSignin.signIn();
       const googleCredential = auth.GoogleAuthProvider.credential(idToken);
       await auth().signInWithCredential(googleCredential);
-      navigation.navigate('MenuPage');
+      navigation.navigate("MenuPage");
     } catch (error) {
       console.error(error);
     }
   }
 
+  async function onSignOutPress() {
+    try {
+      await auth().signOut();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
+
   return (
     <LinearGradient
-      colors={['rgba(255, 1.06, 1.06, 0.20)', 'rgba(208.25, 132.76, 132.76, 0)']}
+      colors={[
+        "rgba(255, 1.06, 1.06, 0.20)",
+        "rgba(208.25, 132.76, 132.76, 0)",
+      ]}
       start={{ x: 0.5, y: 0 }}
       end={{ x: 0.5, y: 1 }}
       style={styles.pageContainer}
@@ -52,12 +62,15 @@ const LoginPage = () => {
       <Text style={styles.text}>Página de Login</Text>
       {user ? (
         <View>
-          <Button title="Sair" />
+          <Button title="Sair" onPress={onSignOutPress} />
         </View>
       ) : (
         <View>
           <Text>Faça login com sua conta do Google:</Text>
-          <GoogleSigninButton style={styles.googleButton} onPress={onGoogleButtonPress} />
+          <GoogleSigninButton
+            style={styles.googleButton}
+            onPress={onGoogleButtonPress}
+          />
         </View>
       )}
     </LinearGradient>
@@ -67,8 +80,8 @@ const LoginPage = () => {
 const styles = StyleSheet.create({
   pageContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   logo: {
     width: 200,
@@ -82,7 +95,7 @@ const styles = StyleSheet.create({
     width: 192,
     height: 48,
     marginTop: 20,
-  }
+  },
 });
 
 export default LoginPage;
